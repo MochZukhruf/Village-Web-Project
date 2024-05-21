cb5b4118766efb96a323b2d1219b477a6eb7a088
MochZukhruf <zukhrufclash@gmail.com>
2024-05-21 16:06:15 +0700

    dropdownnya kagak mau nutup

--- src/components/pages/profile/Aboutus.jsx
+++ src/components/pages/profile/Aboutus.jsx
@@ -1,7 +1,91 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../../styles/profile.css";
 
 export const Aboutus = () => {
   return (
-    <div>Aboutus</div>
-  )
-}
+    <div className="mx-96">
+      <div className="text-sm breadcrumbs">
+        <ul>
+          <li>
+            <div className="img-icon">
+              <Link to="/">
+                <img
+                  className="home-icon"
+                  src="/img/icon/home.png"
+                  alt="Home"
+                />
+              </Link>
+            </div>
+          </li>
+          <li>
+            <a>About</a>
+          </li>
+        </ul>
+      </div>
+      <div className="content-about">
+        <div className="header-content">
+          <h1>About This Platform</h1>
+        </div>
+        <div className="content-fill">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor
+            laborum accusamus odio placeat corrupti nulla amet nobis? Esse
+            voluptas qui sint voluptates mollitia repellat, alias voluptate
+            dolore eveniet, ipsam ab.
+          </p>
+        </div>
+        <div className="header-content">
+          <h1>About This Platform</h1>
+        </div>
+        <div className="content-fill">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor
+            laborum accusamus odio placeat corrupti nulla amet nobis? Esse
+            voluptas qui sint voluptates mollitia repellat, alias voluptate
+            dolore eveniet, ipsam ab.
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor
+            laborum accusamus odio placeat corrupti nulla amet nobis? Esse
+            voluptas qui sint voluptates mollitia repellat, alias voluptate
+            dolore eveniet, ipsam ab.
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor
+            laborum accusamus odio placeat corrupti nulla amet nobis? Esse
+            voluptas qui sint voluptates mollitia repellat, alias voluptate
+            dolore eveniet, ipsam ab.
+          </p>
+        </div>
+        <div className="header-content">
+          <h1>About This Platform</h1>
+        </div>
+        <div className="content-fill">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor
+            laborum accusamus odio placeat corrupti nulla amet nobis? Esse
+            voluptas qui sint voluptates mollitia repellat, alias voluptate
+            dolore eveniet, ipsam ab.
+          </p>
+        </div>
+        <div className="content-fill">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor
+            laborum accusamus odio placeat corrupti nulla amet nobis? Esse
+            voluptas qui sint voluptates mollitia repellat, alias voluptate
+            dolore eveniet, ipsam ab.
+          </p>
+        </div>
+        <div className="content-fill">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor
+            laborum accusamus odio placeat corrupti nulla amet nobis? Esse
+            voluptas qui sint voluptates mollitia repellat, alias voluptate
+            dolore eveniet, ipsam ab.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
