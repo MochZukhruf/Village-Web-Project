5ab1051a3dbfefe411b38c6c3a6490c8c5cd613a
MochZukhruf <zukhrufclash@gmail.com>
2024-06-03 14:29:10 +0700

    bio

--- src/components/pages/goverment/perangkatdesa/Profilstaff.jsx
+++ src/components/pages/goverment/perangkatdesa/Profilstaff.jsx
@@ -30,38 +30,61 @@ export const Profilstaff = () => {
         <div className="profile-staff-content ">
           <div className="profile-staff-box-left">
             <div className="card-content-staff bg-white p-4 shadow-lg rounded-lg">
-              <div className="avatar flex items-center justify-center mb-4">
-                <div className="rounded-xl overflow-hidden border-4 border-gray-200">
-                  <img
-                    src="/img/structure/images1.jpg"
-                    alt="Profile Picture"
-                    className="w-32 h-32 object-cover"
-                  />
+              <div>
+                <div className="avatar flex items-center justify-center mb-4">
+                  <div className="rounded-xl overflow-hidden border-4 border-gray-200">
+                    <img
+                      src="/img/structure/images1.jpg"
+                      alt="Profile Picture"
+                      className="w-32 h-32 object-cover"
+                    />
+                  </div>
+                </div>
+                <div className="text-center">
+                  <table className="w-full table table-profile-staff">
+                    <tbody>
+                      <div>
+                        <tr>
+                          <td>Nama &emsp; &emsp; &nbsp;</td>
+                          <td> : &nbsp; Kades Kotanagaya</td>
+                        </tr>
+                      </div>
+                      <div>
+                        <tr>
+                          <td>Jabatan&emsp; &emsp; </td>
+                          <td> : &nbsp; Kepala Desa</td>
+                        </tr>
+                      </div>
+                      <div>
+                        <tr>
+                          <td>NIP &emsp; &emsp;&emsp; &nbsp; </td>
+                          <td> : &nbsp; -</td>
+                        </tr>
+                      </div>
+                    </tbody>
+                  </table>
+                  <div></div>
                 </div>
               </div>
-              <div className="text-center">
-                <table className="w-full table table-profile-staff">
-                  <tbody>
-                    <div>
-                      <tr>
-                        <td>Nama &emsp; &emsp; &nbsp;</td>
-                        <td> : &nbsp; Kades Kotanagaya</td>
-                      </tr>
-                    </div>
-                    <div>
-                      <tr>
-                        <td>Jabatan&emsp; &emsp; </td>
-                        <td> : &nbsp; Kepala Desa</td>
-                      </tr>
-                    </div>
-                    <div>
-                      <tr>
-                        <td>NIP &emsp; &emsp;&emsp; &nbsp; </td>
-                        <td> : &nbsp; -</td>
-                      </tr>
-                    </div>
-                  </tbody>
-                </table>
+              <div className="bio-profile">
+                <div className="content-title">Biodata</div>
+                <div className="content-fill">
+                  <p>
+                    Kepala desa (disingkat kades) atau petinggi desa adalah
+                    pejabat pemerintah desa yang mempunyai wewenang, tugas dan
+                    kewajiban untuk menyelenggarakan pembangunan desa,
+                    pengembangan masyarakat desa, pemberdayaan masyarakat desa
+                    dan melaksanakan tugas dari Pemerintah. Masa jabatan
+                    kepala desa adalah 6 (enam) tahun, dan dapat diperpanjang
+                    lagi untuk 3 (tiga) kali masa jabatan berikutnya
+                    berturut-turut atau tidak berturut-turut. Kepala desa
+                    tidak bertanggungjawab kepada camat, tetapi hanya
+                    dikoordinasikan saja oleh camat. Kepala desa
+                    bertanggungjawab atas penyelenggarakan pemerintahan desa,
+                    pelaksanakan pembangunan desa, pembinaan kemasyarakatan
+                    desa, dan pemberdayaan masyarakat desa.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
