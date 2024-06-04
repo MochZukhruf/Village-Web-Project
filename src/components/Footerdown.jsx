6cdc534cabfdf68c0045b2b692f0dc26ee81390f
MochZukhruf <zukhrufclash@gmail.com>
2024-06-04 12:46:34 +0700

    Update Footerdown.jsx

--- src/components/Footerdown.jsx
+++ src/components/Footerdown.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { FaPhone, FaEarthAsia } from "react-icons/fa6";
-import { IoMailOutline } from "react-icons/io5";
 
 export const Footerdown = () => {
   return (
